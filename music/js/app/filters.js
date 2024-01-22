f5a4a4716e855794baff2ea9670025ce85c66078
Alex-James <mac@Alexs-MBP.broadband>
2024-01-22 20:07:43 +0000

    Deploying updates

--- music/js/app/filters.js
+++ music/js/app/filters.js
@@ -15,7 +15,45 @@ define(["jquery", "jquery-ui"], function($) {
 	//Filter remaining filters
 	function applyFiltersToFilters(activeFilters)
 	{
+		let availableFilters = {
+			genre: [],
+			decade: [],
+			artist: []
+		};
 
+		$('#coverflow .album').each(function() {
+			const $album = $(this);
+			const genre = $album.attr('data-filter-genre');
+			const decade = $album.attr('data-filter-decade');
+			const artist = $album.attr('data-filter-artist-id');
+
+			if(availableFilters.genre.indexOf(genre) === -1){
+				availableFilters.genre.push(genre);
+			}
+
+			if(availableFilters.decade.indexOf(decade) === -1){
+				availableFilters.decade.push(decade);
+			}
+
+			if(availableFilters.artist.indexOf(artist) === -1){
+				availableFilters.artist.push(artist);
+			}
+		});
+
+		$('#filter-genre .filter-option').hide();
+		for (i in availableFilters.genre) {
+			$('#filter-genre .filter-option[data-code="' + availableFilters.genre[i] + '"]').show();
+		}
+
+		$('#filter-decade .filter-option').hide();
+		for (i in availableFilters.decade) {
+			$('#filter-decade .filter-option[data-code="' + availableFilters.decade[i] + '"]').show();
+		}
+
+		$('#filter-artist .filter-option').hide();
+		for (i in availableFilters.artist) {
+			$('#filter-artist .filter-option[data-code="' + availableFilters.artist[i] + '"]').show();
+		}
 	}
 
 	function updateActiveFilters()
