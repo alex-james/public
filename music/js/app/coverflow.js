5005ab1e0288c08dab17719b6a4f21c72e5a5722
Alex Lancaster <alex@cogensoft.co.uk>
2024-01-29 19:39:36 +0000

    updates

--- music/js/app/coverflow.js
+++ music/js/app/coverflow.js
@@ -8,8 +8,11 @@ define(["jquery", "coverflow"], function($) {
 	const $coverflow = $('#coverflow');
 
 	function buildCoverFlow() {
+		const numVisibleAlbums = $('#coverflow .album').length;
+		if(numVisibleAlbums === 0) return;
+
 		$coverflow.coverflow({
-			active: 4,
+			active: (Math.floor(numVisibleAlbums / 2)),
 			scale: 0.6,
 			select: function(event, ui){
 				const $activeAlbum = $(ui.active);
@@ -27,39 +30,73 @@ define(["jquery", "coverflow"], function($) {
 
 	function applyFiltersToCoverflow(rebuild = false)
 	{
-		if(rebuild) $coverflow.coverflow('destroy');
+		const $albums = $('#coverflow .album');
+		const $unfilteredAlbums = $('#coverflow-unfiltered .album');
+
+		if($albums.length && rebuild) $coverflow.coverflow('destroy');
 		$coverflow.empty();
 
+		let unfilteredStartNumber = Math.floor(Math.random() * $unfilteredAlbums.length);
 		let albumNum = 1;
-		$('#coverflow-unfiltered .album').each(function(){
-			const $album = $(this).clone(false);
-			let show = true;
 
-			switch(document.activeFilters.type) {
-				case 'favourites':
-					show = $album.attr('data-filter-favourite') === '1';
-					break;
-				case 'recent':
-					show = $album.attr('data-filter-recent-added') === '1';
-					break;
+		//Cut the deck
+		$unfilteredAlbums.each(function(){
+			if(albumNum >= unfilteredStartNumber) {
+				addUnfilteredAlbumToCoverflow($(this));
 			}
-
-			if(show && document.activeFilters.genre.length && document.activeFilters.genre.indexOf($album.attr('data-filter-genre')) === -1) {
-				show = false;
-			}
-
-			if(show && document.activeFilters.decade.length && document.activeFilters.decade.indexOf($album.attr('data-filter-decade')) === -1) {
-				show = false;
-			}
-
-			if(show && document.activeFilters.artist.length && document.activeFilters.artist.indexOf($album.attr('data-filter-artist-id')) === -1) {
-				show = false;
-			}
-
-			if(show) $album.attr('src', $album.attr('data-src')).attr('data-album-num', albumNum++).appendTo($coverflow);
+			albumNum++;
 		});
+		albumNum = 1;
+		$unfilteredAlbums.each(function(){
+			if(albumNum < unfilteredStartNumber) {
+				addUnfilteredAlbumToCoverflow($(this));
+			}
+			albumNum++;
+		});
+
+		albumNum = 1;
+		$albums.each(function(){
+			$(this).attr('data-album-num', albumNum++);
+		});
+
 		$('#slider .slider-button').attr('max', $coverflow.children().length);
 		$coverflow.trigger('init');
+	}
+
+	function addUnfilteredAlbumToCoverflow($unfilteredAlbum) {
+		const albumsAdded = $('#coverflow .album').length;
+		const $album = $unfilteredAlbum.clone(false);
+		let show = true;
+
+		switch(document.activeFilters.type) {
+			case 'favourites':
+				show = $album.attr('data-filter-favourite') === '1';
+				break;
+			case 'recent':
+				show = $album.attr('data-filter-recent-added') === '1';
+				break;
+		}
+
+		if(show && document.activeFilters.genre.length && document.activeFilters.genre.indexOf($album.attr('data-filter-genre')) === -1) {
+			show = false;
+		}
+
+		if(show && document.activeFilters.decade.length && document.activeFilters.decade.indexOf($album.attr('data-filter-decade')) === -1) {
+			show = false;
+		}
+
+		if(show && document.activeFilters.artist.length && document.activeFilters.artist.indexOf($album.attr('data-filter-artist-id')) === -1) {
+			show = false;
+		}
+
+		if(show) {
+			$album.attr('src', $album.attr('data-src'));
+			if(albumsAdded === 0) {
+				$('#coverflow').append($album);
+			}
+			position = 1; //Math.floor(Math.random() * albumsAdded) + 1;
+			$('#coverflow > .album:nth-child(' + position + ')').after($album);
+		}
 	}
 
 	applyFiltersToCoverflow(false);
@@ -72,7 +109,7 @@ define(["jquery", "coverflow"], function($) {
 		$('#coverflow').coverflow('next');
 	});
 
-	$('.cover-button').on('click', function() {
+	$('.cover-button.left, .cover-button.right').on('click', function() {
 		const $button = $(this);
 
 		$('#actions .cover-button').removeClass('active');
@@ -83,6 +120,24 @@ define(["jquery", "coverflow"], function($) {
 		} else {
 			$('.info-panel-wrapper').addClass('show-back');
 		}
+	});
+
+	async function handleHardReload(url) {
+		await fetch(url, {
+			headers: {
+				Pragma: 'no-cache',
+				Expires: '-1',
+				'Cache-Control': 'no-cache',
+			},
+		});
+		window.location.href = url;
+		// This is to ensure reload with url's having '#'
+		window.location.reload();
+	}
+
+	$('.cover-button.top').on('click', function() {
+		$(this).addClass('active');
+		handleHardReload(window.location.href);
 	});
 
 	$coverflow.on('refresh', function() {
--- music/js/app/filters.js
+++ music/js/app/filters.js
@@ -3,56 +3,27 @@ define(["jquery", "jquery-ui"], function($) {
 	$('#filter-tabs').tabs();
 
 	//Filters Selection
-	$('.filter-option, .filter-type').on('click', function(){
+	$('.filter-option, .filter-type, .filter-option-all').on('click', function(){
 		const $filterOption = $(this);
 
+		setAllFilterStatus($filterOption);
 		$filterOption.attr('data-active', $filterOption.attr('data-active') === '0' ? '1' : '0');
 		updateActiveFilters()
 		$('#coverflow').trigger('refresh');
 		applyFiltersToFilters();
 	});
 
-	//Filter remaining filters
-	function applyFiltersToFilters(activeFilters)
+	function setAllFilterStatus($filterOption)
 	{
-		let availableFilters = {
-			genre: [],
-			decade: [],
-			artist: []
-		};
+		const $filterOptions = $filterOption.closest('.filter-options');
+		const activeFiltersForGenre = $filterOptions.find('.filter-option[data-active="1"]').length;
 
-		$('#coverflow .album').each(function() {
-			const $album = $(this);
-			const genre = $album.attr('data-filter-genre');
-			const decade = $album.attr('data-filter-decade');
-			const artist = $album.attr('data-filter-artist-id');
-
-			if(availableFilters.genre.indexOf(genre) === -1){
-				availableFilters.genre.push(genre);
-			}
-
-			if(availableFilters.decade.indexOf(decade) === -1){
-				availableFilters.decade.push(decade);
-			}
-
-			if(availableFilters.artist.indexOf(artist) === -1){
-				availableFilters.artist.push(artist);
-			}
-		});
-
-		$('#filter-genre .filter-option').hide();
-		for (i in availableFilters.genre) {
-			$('#filter-genre .filter-option[data-code="' + availableFilters.genre[i] + '"]').show();
-		}
-
-		$('#filter-decade .filter-option').hide();
-		for (i in availableFilters.decade) {
-			$('#filter-decade .filter-option[data-code="' + availableFilters.decade[i] + '"]').show();
-		}
-
-		$('#filter-artist .filter-option').hide();
-		for (i in availableFilters.artist) {
-			$('#filter-artist .filter-option[data-code="' + availableFilters.artist[i] + '"]').show();
+		if($filterOption.hasClass('filter-option-all') && activeFiltersForGenre) {
+			$filterOptions.removeClass('has-active-filters');
+			$filterOptions.find('.filter-option').attr('data-active', 0);
+		} else {
+			$filterOptions.addClass('has-active-filters');
+			$filterOptions.find('.filter-option-all').attr('data-active', (activeFiltersForGenre > 0 ? '0' : '1'));
 		}
 	}
 
@@ -77,5 +48,116 @@ define(["jquery", "jquery-ui"], function($) {
 		$filterTable.find('#filter-artist .filter-option[data-active="1"]').each(function(){
 			document.activeFilters.artist.push($(this).attr('data-code'));
 		});
+	}
+
+	function buildDataSelector(filterType, filterValue) {
+		let selector;
+
+		switch(filterType) {
+			case 'genre':
+				selector = 'genre';
+				break
+			case 'decade':
+				selector = 'decade';
+				break
+			case 'artist':
+				selector = 'artist-id';
+				break;
+		}
+
+		return '[data-filter-' + selector + '="' + filterValue + '"]';
+	}
+
+	//Filter remaining filters
+	function applyFiltersToFilters()
+	{
+		let availableFilters = {
+			genre:[],
+			decade: [],
+			artist: []
+		};
+
+		['genre', 'decade', 'artist'].forEach(function(primaryFilterType){
+			let selectors= [];
+			let filterValue, primaryDataSelector, secondaryDataSelector, tertiaryDataSelector, secondaryFilterTypes;
+			for (i in document.activeFilters[primaryFilterType]) {
+				filterValue = document.activeFilters[primaryFilterType][i];
+				primaryDataSelector = buildDataSelector(primaryFilterType, filterValue);
+
+				switch(primaryFilterType) {
+					case 'genre':
+						secondaryFilterTypes = ['decade', 'artist'];
+						break
+					case 'decade':
+						secondaryFilterTypes = ['genre', 'artist'];
+						break
+					case 'artist':
+						secondaryFilterTypes = ['genre', 'decade'];
+						break;
+				}
+
+				if(document.activeFilters[secondaryFilterTypes[0]].length && document.activeFilters[secondaryFilterTypes[1]].length) {
+					for (i in document.activeFilters[secondaryFilterTypes[0]]) {
+						secondaryDataSelector = buildDataSelector(secondaryFilterTypes[0], document.activeFilters[secondaryFilterTypes[0]][i]);
+						for (i in document.activeFilters[secondaryFilterTypes[1]]) {
+							tertiaryDataSelector = buildDataSelector(secondaryFilterTypes[1], document.activeFilters[secondaryFilterTypes[1]][i]);
+							selectors.push(primaryDataSelector+secondaryDataSelector+tertiaryDataSelector);
+						}
+					}
+				} else if(document.activeFilters[secondaryFilterTypes[0]].length) {
+					for (i in document.activeFilters[secondaryFilterTypes[0]]) {
+						secondaryDataSelector = buildDataSelector(secondaryFilterTypes[0], document.activeFilters[secondaryFilterTypes[0]][i]);
+						selectors.push(primaryDataSelector+secondaryDataSelector);
+					}
+				} else if(document.activeFilters[secondaryFilterTypes[1]].length) {
+					for (i in document.activeFilters[secondaryFilterTypes[1]]) {
+						tertiaryDataSelector = buildDataSelector(secondaryFilterTypes[1], document.activeFilters[secondaryFilterTypes[1]][i]);
+						selectors.push(primaryDataSelector+tertiaryDataSelector);
+					}
+				} else {
+					selectors.push(primaryDataSelector);
+				}
+
+				selectors.forEach(function(selector){
+					$('#coverflow-unfiltered .album' + selector).each(function(){
+						const $album = $(this);
+						const genre = $album.attr('data-filter-genre');
+						const decade = $album.attr('data-filter-decade');
+						const artist = $album.attr('data-filter-artist-id');
+
+						if(availableFilters.genre.indexOf(genre) === -1) {
+							availableFilters.genre.push(genre);
+						}
+						if(availableFilters.decade.indexOf(decade) === -1) {
+							availableFilters.decade.push(decade);
+						}
+						if(availableFilters.artist.indexOf(artist) === -1) {
+							availableFilters.artist.push(artist);
+						}
+					})
+				})
+			}
+		});
+
+		if(!$('#filter-genre .filter-options').hasClass('has-active-filters')) {
+			$('#filter-genre .filter-option').hide();
+			for (i in availableFilters.genre) {
+				$('#filter-genre .filter-option[data-code="' + availableFilters.genre[i] + '"]').show();
+			}
+		}
+
+		if(!$('#filter-decade .filter-options').hasClass('has-active-filters')) {
+			$('#filter-decade .filter-option').hide();
+			for (i in availableFilters.decade) {
+				$('#filter-decade .filter-option[data-code="' + availableFilters.decade[i] + '"]').show();
+			}
+		}
+
+		if(!$('#filter-artist .filter-options').hasClass('has-active-filters')) {
+			$('#filter-artist .filter-option').hide();
+			for (i in availableFilters.artist) {
+				$('#filter-artist .filter-option[data-code="' + availableFilters.artist[i] + '"]').show();
+			}
+		}
 	}
 });
